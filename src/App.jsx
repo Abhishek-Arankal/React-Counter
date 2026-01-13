60d908117e0762da605aeab8d8b29f9f31728c9a
Abhishek Arankal <abhishekarankal55837@gmail.com>
2026-01-13 20:20:30 +0530

    Level 3: Maximum Constraint and Disable Increment Button

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleIncrement = () => {
+    console.log("clicked")
+    if(count < 10){
+      setCount(count + 1)
+    }
+  }
+
   const handleDecrement = () => {
     console.log("clicked")
     if(count > 0) {
@@ -17,7 +24,7 @@ function App() {
   <div className="min-w-[320px] border rounded-lg flex items-center gap-6 px-10 py-10">
     <div className="w-24 h-24 border rounded-full flex items-center justify-center text-2xl font-bold">{count}</div>
     <div className="flex flex-col gap-3">
-      <button className="px-4 py-2 bg-green-600 rounded hover:bg-green-700 transition" onClick={() => setCount(count +1)}>Increment</button>
+      <button className="px-4 py-2 bg-green-600 rounded hover:bg-green-700 transition" onClick={() => handleIncrement()} disabled={count === 10}>Increment</button>
       <button className="px-4 py-2 bg-red-600 rounded hover:bg-red-700 transition"onClick={() => handleDecrement()} disabled={count === 0}>Decrement</button>
     </div>
 
