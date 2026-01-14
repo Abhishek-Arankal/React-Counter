be3f75a7d4fbd2a6b933eba4ed9694b6c0fe837a
Abhishek Arankal <abhishekarankal55837@gmail.com>
2026-01-14 10:34:06 +0530

    Level 8: Fix increment logic for multiple counters

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,47 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counters, setCounters] = useState([
+    {id:1, value:0},
+    {id:2, value:0},
+    {id:3, value:0}
+  ])
+console.log("Counter", counters)
 
-  const handleIncrement = () => {
+  const handleIncrement = (id) => {
     console.log("clicked")
-    if(count < 10){
-      setCount(count + 1)
-    }
+    setCounters((prevCounters) => {
+      console.log(prevCounters)
+       return prevCounters.map((counter) => counter.id === id ? {...counter, value: counter.value < 10 ? counter.value + 1 : counter.value} : counter)
+    })
   }
 
-  const handleDecrement = () => {
+  const handleDecrement = (id) => {
     console.log("clicked")
-    if(count > 0) {
-      setCount(count - 1)
-    }
+    setCounters((prevCounters) => {
+      return prevCounters.map((counter) => counter.id === id ? {...counter, value: counter.value > 0 ? counter.value -1 : counter.value} : counter)
+    })
+  }
+
+  const handleReset = (id) => {
+    setCounters((prevCounter) => {
+      return prevCounter.map((counter) => counter.id === id ? {...counter, value: 0} : counter)
+    })
   }
 
   return (
     <div className="w-full h-screen bg-gray-800 text-white flex items-center justify-center">
-  <div className="min-w-[320px] border rounded-lg flex items-center gap-6 px-10 py-10">
-    <div className="w-24 h-24 border rounded-full flex items-center justify-center text-2xl font-bold">{count}</div>
+  {counters.map((counter) => (
+    <div className="min-w-[320px] border rounded-lg flex items-center gap-6 px-10 py-10 m-2" key={counter.id}>
+    <div className="w-24 h-24 border rounded-full flex items-center justify-center text-2xl font-bold">{counter.value}</div>
     <div className="flex flex-col gap-3">
-      <button className="px-4 py-2 bg-green-600 rounded hover:bg-green-700 transition" onClick={() => handleIncrement()} disabled={count === 10}>Increment</button>
-      <button className="px-4 py-2 bg-red-600 rounded hover:bg-red-700 transition"onClick={() => handleDecrement()} disabled={count === 0}>Decrement</button>
-      <button className='px-4 py-2 bg-yellow-600 rounded hover:bg-yellow-700 transition' onClick={() => setCount(0)}>Reset</button>
+      <button className="px-4 py-2 bg-green-600 rounded hover:bg-green-700 transition" onClick={() => handleIncrement(counter.id)} disabled={counter.value === 10}>Increment</button>
+      <button className="px-4 py-2 bg-red-600 rounded hover:bg-red-700 transition"onClick={() => handleDecrement(counter.id)} disabled={counter.value === 0}>Decrement</button>
+      <button className='px-4 py-2 bg-yellow-600 rounded hover:bg-yellow-700 transition'onClick={() => handleReset(counter.id)}>Reset</button>
     </div>
 
   </div>
+  ))}
 </div>
 
   )
